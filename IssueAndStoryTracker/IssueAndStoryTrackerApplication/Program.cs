4d84dc447cbee145b981f21283ddcc1b43500b6e
Jack Hougland <jack.hougland@gmail.com>
2020-01-25 10:30:48 -0600

    Initial check in
    Removed the weather service and counter that are generated by Visual Studio for new Blazor apps.  Modified various files with indentation preferences and added comments to methods automatically provided by Visual Studio.

---
+++ IssueAndStoryTracker/IssueAndStoryTrackerApplication/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace IssueAndStoryTrackerApplication
+{
+  /// <summary>
+  /// This class provides the main method for the app
+  /// and it creates a host builder to get the app going.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// Starter method for the program.
+    /// </summary>
+    /// <param name="args">Collection of arguments for the host builder creation activity..</param>
+    public static void Main( string[] args )
+    {
+      CreateHostBuilder( args ).Build().Run();
+    }
+
+    /// <summary>
+    /// Creates and returns a host builder.
+    /// </summary>
+    /// <param name="args">Collection of arguments for the default builder activity.</param>
+    /// <returns>A host builder.</returns>
+    public static IHostBuilder CreateHostBuilder( string[] args ) =>
+        Host.CreateDefaultBuilder( args )
+            .ConfigureWebHostDefaults( webBuilder =>
+             {
+              webBuilder.UseStartup<Startup>();
+            } );
+  }
+}
---
+++ IssueAndStoryTracker/IssueAndStoryTrackerApplication/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IssueAndStoryTrackerApplication
+{
+  /// <summary>
+  /// This class provides startup support for the app.
+  /// </summary>
+  public class Startup
+  {
+    /// <summary>
+    /// Saves off the given configuration to a property.
+    /// </summary>
+    /// <param name="configuration">Represents key/value properties for the app's configuration.</param>
+    public Startup( IConfiguration configuration )
+    {
+      Configuration = configuration;
+    }
+
+    /// <summary>
+    /// Gets the app's configuration key/value properties.
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to add services to the container.
+    /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    /// </summary>
+    /// <param name="services">Specifies the contract for a collection of service descriptors.</param>
+    public void ConfigureServices( IServiceCollection services )
+    {
+      services.AddRazorPages();
+      services.AddServerSideBlazor();      
+      services.AddServerSideBlazor();      
+    }
+
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// </summary>
+    /// <param name="app">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
+    /// <param name="env">Provides information about the web hosting environment an application is running in.</param>
+    public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler( "/Error" );        
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseEndpoints( endpoints =>
+       {
+         endpoints.MapBlazorHub();
+         endpoints.MapFallbackToPage( "/_Host" );
+       } );
+    }
+  }
+}
